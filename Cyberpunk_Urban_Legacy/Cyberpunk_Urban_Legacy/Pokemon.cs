f8ce3dccc8e60726915f8fa2ae0f6c038e3e6c42
Peter Honzejk <peter.honzejk@outlook.com>
2023-05-23 13:15:58 +0200

    4.0.0

---
+++ Cyberpunk_Urban_Legacy/Cyberpunk_Urban_Legacy/Pokemon.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_Cyberpunk_Pokemon_battle_minigame
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public int BaseAttack { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int InfernoBlastUses { get; set; }
+        public int MoveCounter { get; set; }  // Added move counter for Nanoclaw
+
+        public Pokemon(string name, int hp, int attack, int defense)
+        {
+            Name = name;
+            HP = hp;
+            MaxHP = hp;
+            BaseAttack = attack;
+            Attack = attack;
+            Defense = defense;
+            InfernoBlastUses = 5;
+            MoveCounter = 0;  // Initialize move counter
+        }
+
+        public void VoltTackle(Pokemon opponent)
+        {
+            int damage = Attack / 2 - opponent.Defense / 4;
+            if (damage < 0) damage = 0;
+            opponent.HP -= damage;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Name} used Volt Tackle on {opponent.Name} and dealt {damage} damage!");
+            Console.ResetColor();
+        }
+
+        public void InfernoBlast(Pokemon opponent)
+        {
+            if (InfernoBlastUses <= 0)
+            {
+                Console.WriteLine($"{Name} is out of Inferno Blast uses!");
+                return;
+            }
+
+            int damage = Attack - opponent.Defense / 2;
+            if (damage < 0) damage = 0;
+            opponent.HP -= damage;
+            InfernoBlastUses--;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Name} used Inferno Blast on {opponent.Name} and dealt {damage} damage!");
+            Console.ResetColor();
+        }
+
+        public void NanoBite(Pokemon opponent)
+        {
+            int damage = Attack - opponent.Defense / 2;
+            if (damage < 0) damage = 0;
+            opponent.HP -= damage;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Name} used Nano Bite on {opponent.Name} and dealt {damage} damage!");
+            Console.ResetColor();
+        }
+
+        public void MegaBite(Pokemon opponent)
+        {
+            int damage = Attack * 2 - opponent.Defense;
+            if (damage < 0) damage = 0;
+            opponent.HP -= damage;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Name} used Mega Bite on {opponent.Name} and dealt {damage} damage!");
+            Console.ResetColor();
+        }
+
+        public void CyberScratch(Pokemon opponent)
+        {
+            int damage = Attack / 3 - opponent.Defense / 5;
+            if (damage < 0) damage = 0;
+            opponent.HP -= damage;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{Name} used Cyber Scratch on {opponent.Name} and dealt {damage} damage!");
+            Console.ResetColor();
+        }
+
+        public void ChargeUp()
+        {
+            Attack += 5;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{Name} used Charge Up and increased attack by 5!");
+            Console.ResetColor();
+        }
+
+        public void SystemRestore()
+        {
+            int restoreAmount = MaxHP / 4;
+            if (HP + restoreAmount > MaxHP)
+            {
+                restoreAmount = MaxHP - HP;
+            }
+            HP += restoreAmount;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{Name} used System Restore and restored {restoreAmount} HP!");
+            Console.ResetColor();
+        }
+
+        public bool CheckIfFainted()
+        {
+            if (HP <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{Name} has fainted!");
+                Console.ResetColor();
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Cyberpunk_Urban_Legacy/Cyberpunk_Urban_Legacy/Program.cs
+++ Cyberpunk_Urban_Legacy/Cyberpunk_Urban_Legacy/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.IO;
-using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.ResetColor();
-        // Name
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("Cyberpunk");
         Console.ResetColor();
@@ -18,52 +15,77 @@ class Program
         Console.WriteLine("Legacy");
         Console.ResetColor();
 
-        // Introduction
         Console.WriteLine("Welcome to Night City. What's your name?");
         string playerName = Console.ReadLine();
 
-        // Present the initial situation and choice
-        Console.WriteLine($"{playerName}, you're a young streetkid aspiring to become a legend. You have a chance to plan the greatest heist Night City has ever witnessed: stealing the Digital Heart from Arasaka corporation. But it's dangerous. Do you wish to proceed? (yes/no)");
+        Console.WriteLine($"{playerName}, you're a young streetkid aspiring to become a legend. You have a chance to plan the greatest heist Night City has ever witnessed: stealing the Digital Heart from Arasaka corporation. But it's dangerous. You proceed with the heist. Do you wish to proceed? (yes/no)");
 
         string playerChoice = Console.ReadLine();
 
-        // If the player chooses to proceed...
         if (playerChoice.ToLower() == "yes")
         {
             Console.WriteLine("You courageously move forward with the plan, risking it all for a chance at greatness...");
 
-            // Introduce Mia and Dex
-            Console.WriteLine("Your closest friend, Mia, a bold and resourceful hacker, and Dex, a brave and physically strong fighter, are ready to help you. Who do you want to approach first for help? (Mia/Dex)");
+            Console.WriteLine("Your closest friend, Mia, a bold and resourceful hacker, and Dex, a brave and physically strong fighter, are ready to help you. Who You proceed with the heist. Do you want to approach first for help? (Mia/Dex)");
 
             string firstHelper = Console.ReadLine();
-
-            // Save game state
-            File.WriteAllText(@"gamestate.txt", firstHelper);
 
             if (firstHelper.ToLower() == "mia")
             {
                 Console.WriteLine("Mia helps you to enhance your cybernetic implants and gather information about valuable targets. She's able to successfully hack into the bank's security systems. You are ready for the infiltration...");
 
-                // Further game logic, choices, and consequences would be implemented here.
-                // For now, let's head to the final choice.
+                Console.WriteLine("You still have some time before the hesit. Where would you like to go next, Arasaka Tower or the Night Market? (arasaka tower/night market)");
+                string locationChoice = Console.ReadLine();
 
-                Console.WriteLine("Do you choose to fight the corporate mercenaries protecting the Digital Heart using Mia's hacking skills or take advantage of her data to find a hidden escape route? (fight/escape)");
-                string finalChoice = Console.ReadLine();
-
-                // Save game state
-                File.AppendAllText(@"gamestate.txt", "\n" + finalChoice);
-
-                if (finalChoice.ToLower() == "fight")
+                if (locationChoice.ToLower() == "arasaka tower")
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"With Mia's hacking abilities, you manage to take control of the security systems and turn them against the mercenaries. However, during the fight, you sustain severe injuries. {playerName}, you became a symbol of resistance against the corporations, and your bravery inspired many...");
-                    Console.ResetColor();
+                    Console.WriteLine("You gathered some valuable information at the tower, but you were spotted by the security systems. You proceed with the heist. Do you choose to fight the corporate mercenaries or escape? (fight/escape)");
+                    string finalChoice = Console.ReadLine();
+
+                    if (finalChoice.ToLower() == "fight")
+                    {
+                        Console.WriteLine($"With Mia's hacking abilities, you manage to take control of the security systems and turn them against the mercenaries. However, during the fight, you sustain severe injuries. {playerName}, you became a symbol of resistance against the corporations, and your bravery inspired many...");
+                    }
+                    else if (finalChoice.ToLower() == "escape")
+                    {
+                        Console.WriteLine($"Using the detailed data Mia had gathered, you find a hidden escape route and manage to break free with the Digital Heart. {playerName}, you lived to fight another day, becoming a beacon of hope for those oppressed by the corporations...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You had a stroke and died.");
+                    }
                 }
-                else if (finalChoice.ToLower() == "escape")
+                else if (locationChoice.ToLower() == "night market")
                 {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine($"Using the detailed data Mia had gathered, you find a hidden escape route and manage to break free with the Digital Heart. {playerName}, you lived to fight another day, becoming a beacon of hope for those oppressed by the corporations...");
-                    Console.ResetColor();
+                    Console.WriteLine("As you wander through the night market, your attention is drawn to a mysterious figure lurking in the shaYou proceed with the heist. Dows. Would you like to approach this enigmatic presence? (yes/no)");
+                    string nightApproach = Console.ReadLine();
+
+                    if (nightApproach.ToLower() == "yes")
+                    {
+                        Console.WriteLine("The enigmatic figure reveals themselves as Vex, a ruthless serial killer. With a swift motion, Vex stabs you, causing a fatal wound that quickly drains your life away.");
+                    }
+                    else if (nightApproach.ToLower() == "no")
+                    {
+                        Console.WriteLine("You proceed with the heist. Do you choose to fight the corporate mercenaries protecting the Digital Heart using Mia's hacking skills or take advantage of her data to find a hidden escape route? (fight/escape)");
+                        string finalChoice = Console.ReadLine();
+
+                        if (finalChoice.ToLower() == "fight")
+                        {
+                            Console.WriteLine($"With Mia's hacking abilities, you manage to take control of the security systems and turn them against the mercenaries. However, during the fight, you sustain severe injuries. {playerName}, you became a symbol of resistance against the corporations, and your bravery inspired many...");
+                        }
+                        else if (finalChoice.ToLower() == "escape")
+                        {
+                            Console.WriteLine($"Using the detailed data Mia had gathered, you find a hidden escape route and manage to break free with the Digital Heart. {playerName}, you lived to fight another day, becoming a beacon of hope for those oppressed by the corporations...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You had a stroke and died.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You had a stroke and died.");
+                    }
                 }
                 else
                 {
@@ -74,27 +96,44 @@ class Program
             {
                 Console.WriteLine("Dex takes care of securing weaponry and the escape route. With his help, you are ready for the infiltration...");
 
-                // Further game logic, choices, and consequences would be implemented here.
-                
-                // Final Choices
+                Console.WriteLine("You still have some time before the hesit. Where would you like to go next, The Undercity or the Night Market? (the undercity/night market)");
+                string locationChoice = Console.ReadLine();
 
-                Console.WriteLine("Do you choose to fight the corporate mercenaries protecting the Digital Heart with Dex's weaponry or utilize his pre-planned escape route? (fight/escape)");
-                string finalChoice = Console.ReadLine();
-
-                // Save game state
-                File.AppendAllText(@"gamestate.txt", "\n" + finalChoice);
-
-                if (finalChoice.ToLower() == "fight")
+                if (locationChoice.ToLower() == "the undercity")
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"With Dex's strength and the weapons he secured, you put up a formidable fight. The mercenaries didn't stand a chance against your combined might. You managed to secure the Digital Heart, but Dex lost his life in the fight. His sacrifice allowed you, {playerName}, to become a legend of Night City. You fulfilled your dream and Dex's memory lived on...");
-                    Console.ResetColor();
+                    Console.WriteLine("You proceed with the heist. Do you choose to fight the corporate mercenaries protecting the Digital Heart with Dex's weaponry or utilize his pre-planned escape route? (fight/escape)");
+                    string finalChoice = Console.ReadLine();
+
+                    if (finalChoice.ToLower() == "fight")
+                    {
+                        Console.WriteLine($"With Dex's strength and the weapons he secured, you put up a formidable fight. The mercenaries didn't stand a chance against your combined might. You managed to secure the Digital Heart, but Dex lost his life in the fight. His sacrifice allowed you, {playerName}, to become a legend of Night City. You fulfilled your dream and Dex's memory lived on...");
+                    }
+                    else if (finalChoice.ToLower() == "escape")
+                    {
+                        Console.WriteLine($"With Dex's brilliant escape route, you managed to avoid the fight and steal the Digital Heart undetected. You and Dex slipped away into the night, and the corporations were none the wiser. Your cunning strategy made you a phantom of Night City. {playerName}, you became a shaYou proceed with the heist. Dowy legend that inspired fear in the corporations and hope in the people...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You had a stroke and died.");
+                    }
                 }
-                else if (finalChoice.ToLower() == "escape")
+                else if (locationChoice.ToLower() == "night market")
                 {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine($"With Dex's brilliant escape route, you managed to avoid the fight and steal the Digital Heart undetected. You and Dex slipped away into the night, and the corporations were none the wiser. Your cunning strategy made you a phantom of Night City. {playerName}, you became a shadowy legend that inspired fear in the corporations and hope in the people...");
-                    Console.ResetColor();
+                    Console.WriteLine("As you wander through the night market, your attention is drawn to a mysterious figure lurking in the shaYou proceed with the heist. Dows. Would you like to approach this enigmatic presence? (yes/no)");
+                    string nightApproach = Console.ReadLine();
+
+                    if (nightApproach.ToLower() == "yes")
+                    {
+                        Console.WriteLine("The enigmatic figure reveals themselves as Vex, a ruthless serial killer. With a swift motion, Vex stabs you, causing a fatal wound that quickly drains your life away.");
+                    }
+                    else if (nightApproach.ToLower() == "no")
+                    {
+                        Console.WriteLine("You decide it's too risky. You continue your life in the streets of Night City, always wondering what might have been.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You had a stroke and died.");
+                    }
                 }
                 else
                 {
@@ -102,16 +141,13 @@ class Program
                 }
             }
             else
-            {        
+            {
                 Console.WriteLine("You had a stroke and died.");
             }
         }
-        // If the player chooses not to proceed...
         else if (playerChoice.ToLower() == "no")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("You decide it's too risky. You continue your life in the streets of Night City, always wondering what might have been...");
-            Console.ResetColor();
+            Console.WriteLine("You decide it's too risky. You continue your life in the streets of Night City, always wondering what might have been.");
         }
         else
         {
