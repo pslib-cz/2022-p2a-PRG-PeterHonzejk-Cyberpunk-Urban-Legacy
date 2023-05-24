7d67ad0e1d596b5c7086da99cabc391ae713abf3
Peter Honzejk <peter.honzejk@outlook.com>
2023-05-24 09:41:10 +0200

    4.4.0

--- Cyberpunk_Urban_Legacy/Cyberpunk_Urban_Legacy/Program.cs
+++ Cyberpunk_Urban_Legacy/Cyberpunk_Urban_Legacy/Program.cs
@@ -23,37 +23,26 @@ class Program
         string playerChoice = Console.ReadLine();
 
         if (playerChoice.ToLower() == "yes")
+        if (playerChoice.ToLower() == "yes")
         {
-            Console.WriteLine("You courageously move forward with the plan, risking it all for a chance at greatness...");
+            Console.WriteLine("You courageously move forward with the plan, risking it all for a chance at greatness.");
 
-            Console.WriteLine("Your closest friend, Mia, a bold and resourceful hacker, and Dex, a brave and physically strong fighter, are ready to help you. Who You proceed with the heist. Do you want to approach first for help? (Mia/Dex)");
+            Console.WriteLine("Your closest friend, Mia, a bold and resourceful hacker, and Dex, a brave and physically strong fighter, are ready to help you. Who do you want to approach first for help? (Mia/Dex)");
 
             string firstHelper = Console.ReadLine();
 
             if (firstHelper.ToLower() == "mia")
             {
-                Console.WriteLine("Mia helps you to enhance your cybernetic implants and gather information about valuable targets. She's able to successfully hack into the bank's security systems. You are ready for the infiltration...");
+                Console.WriteLine("Mia helps you to enhance your cybernetic implants and gather information about valuable targets. She's able to successfully hack into the bank's security systems.");
 
                 Console.WriteLine("You still have some time before the hesit. Where would you like to go next, Arasaka Tower or the Night Market? (arasaka tower/night market)");
                 string locationChoice = Console.ReadLine();
 
                 if (locationChoice.ToLower() == "arasaka tower")
                 {
-                    Console.WriteLine("You gathered some valuable information at the tower, but you were spotted by the security systems. You proceed with the heist. Do you choose to fight the corporate mercenaries or escape? (fight/escape)");
+                    Console.WriteLine("");
                     string finalChoice = Console.ReadLine();
-
-                    if (finalChoice.ToLower() == "fight")
-                    {
-                        Console.WriteLine($"With Mia's hacking abilities, you manage to take control of the security systems and turn them against the mercenaries. However, during the fight, you sustain severe injuries. {playerName}, you became a symbol of resistance against the corporations, and your bravery inspired many...");
-                    }
-                    else if (finalChoice.ToLower() == "escape")
-                    {
-                        Console.WriteLine($"Using the detailed data Mia had gathered, you find a hidden escape route and manage to break free with the Digital Heart. {playerName}, you lived to fight another day, becoming a beacon of hope for those oppressed by the corporations...");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You had a stroke and died.");
-                    }
+                    // change pokemon battle minigame good ending (help from horse) / death ending (tiger kills you)
                 }
                 else if (locationChoice.ToLower() == "night market")
                 {
@@ -63,6 +52,7 @@ class Program
                     if (nightApproach.ToLower() == "yes")
                     {
                         Console.WriteLine("The enigmatic figure reveals themselves as Vex, a ruthless serial killer. With a swift motion, Vex stabs you, causing a fatal wound that quickly drains your life away.");
+                    // change to tictactoe minigame good ending (vex helps you) / death ending (vex stabs you)
                     }
                     else if (nightApproach.ToLower() == "no")
                     {
@@ -71,11 +61,11 @@ class Program
 
                         if (finalChoice.ToLower() == "fight")
                         {
-                            Console.WriteLine($"With Mia's hacking abilities, you manage to take control of the security systems and turn them against the mercenaries. However, during the fight, you sustain severe injuries. {playerName}, you became a symbol of resistance against the corporations, and your bravery inspired many...");
+                            Console.WriteLine($"With Mia's hacking abilities, you manage to take control of the security systems and turn them against the mercenaries. However, during the fight, you sustain severe injuries. {playerName}, you became a symbol of resistance against the corporations, and your bravery inspired many.");
                         }
                         else if (finalChoice.ToLower() == "escape")
                         {
-                            Console.WriteLine($"Using the detailed data Mia had gathered, you find a hidden escape route and manage to break free with the Digital Heart. {playerName}, you lived to fight another day, becoming a beacon of hope for those oppressed by the corporations...");
+                            Console.WriteLine($"Using the detailed data Mia had gathered, you find a hidden escape route and manage to break free with the Digital Heart.  Mia sacrifices herself, allowing you, {playerName}, to escape and become a symbol of resistance in Night City, carrying on her legacy.");
                         }
                         else
                         {
@@ -94,42 +84,45 @@ class Program
             }
             else if (firstHelper.ToLower() == "dex")
             {
-                Console.WriteLine("Dex takes care of securing weaponry and the escape route. With his help, you are ready for the infiltration...");
+                Console.WriteLine("Dex takes care of securing weaponry and the escape route. With his help, you are ready for the infiltration.");
 
-                Console.WriteLine("You still have some time before the hesit. Where would you like to go next, The Undercity or the Night Market? (the undercity/night market)");
+                Console.WriteLine("You still have some time before the heist. Where would you like to go next, The Undercity or the Night Market? (the undercity/night market)");
                 string locationChoice = Console.ReadLine();
 
                 if (locationChoice.ToLower() == "the undercity")
                 {
-                    Console.WriteLine("You proceed with the heist. Do you choose to fight the corporate mercenaries protecting the Digital Heart with Dex's weaponry or utilize his pre-planned escape route? (fight/escape)");
-                    string finalChoice = Console.ReadLine();
-
-                    if (finalChoice.ToLower() == "fight")
-                    {
-                        Console.WriteLine($"With Dex's strength and the weapons he secured, you put up a formidable fight. The mercenaries didn't stand a chance against your combined might. You managed to secure the Digital Heart, but Dex lost his life in the fight. His sacrifice allowed you, {playerName}, to become a legend of Night City. You fulfilled your dream and Dex's memory lived on...");
-                    }
-                    else if (finalChoice.ToLower() == "escape")
-                    {
-                        Console.WriteLine($"With Dex's brilliant escape route, you managed to avoid the fight and steal the Digital Heart undetected. You and Dex slipped away into the night, and the corporations were none the wiser. Your cunning strategy made you a phantom of Night City. {playerName}, you became a shaYou proceed with the heist. Dowy legend that inspired fear in the corporations and hope in the people...");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You had a stroke and died.");
-                    }
+                // change to hacking minigame good ending (you gain access to arasaka network) / death ending (your brain is fried)
                 }
                 else if (locationChoice.ToLower() == "night market")
                 {
-                    Console.WriteLine("As you wander through the night market, your attention is drawn to a mysterious figure lurking in the shaYou proceed with the heist. Dows. Would you like to approach this enigmatic presence? (yes/no)");
+                    Console.WriteLine("As you wander through the night market, your attention is drawn to a mysterious figure lurking in the shadows. Would you like to approach this enigmatic presence? (yes/no)");
                     string nightApproach = Console.ReadLine();
 
                     if (nightApproach.ToLower() == "yes")
                     {
                         Console.WriteLine("The enigmatic figure reveals themselves as Vex, a ruthless serial killer. With a swift motion, Vex stabs you, causing a fatal wound that quickly drains your life away.");
+                    // change to tictactoe minigame good ending (vex helps you) / death ending (vex stabs you)
                     }
                     else if (nightApproach.ToLower() == "no")
                     {
-                        Console.WriteLine("You decide it's too risky. You continue your life in the streets of Night City, always wondering what might have been.");
-                    }
+                            Console.WriteLine("You proceed with the heist but after securing the Digital Hear youre ambushed by Arasaka's mercenaries. Do you choose to fight the corporate mercenaries with Dex's weaponry or utilize his pre-planned escape route? (fight/escape)");
+                            string finalChoice = Console.ReadLine();
+
+                            if (finalChoice.ToLower() == "fight")
+                            {
+                                Console.WriteLine($"With Dex's strength and the weapons he secured, you put up a formidable fight. The mercenaries didn't stand a chance against your combined might. You managed to secure the Digital Heart, but Dex lost his life in the fight. His sacrifice allowed you, {playerName}, to become a legend of Night City. You fulfilled your dream and Dex's memory lived on.");
+
+                            }
+                            else if (finalChoice.ToLower() == "escape")
+                            {
+                                Console.WriteLine($"With Dex's brilliant escape route, you managed to avoid the fight and steal the Digital Heart undetected. You and Dex slipped away into the night, and the corporations were none the wiser. Your cunning strategy made you a phantom of Night City. {playerName}, you became a shadowy legend that inspired fear in the corporations and hope in the people.");
+                                //change to player death ending (legend status)
+                            }
+                            else
+                            {
+                                Console.WriteLine("You had a stroke and died.");
+                            }
+                        }
                     else
                     {
                         Console.WriteLine("You had a stroke and died.");
